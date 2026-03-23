536339afc5c864876af7a6a02551ea45002252b0
James <jamesdev4you@gmail.com>
2026-03-23 11:56:52 -0400

    starting to use tailwind

---
+++ src/app/components/home/HomeHeader.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const HomeHeader = () => {
+  return (
+    <div className="w-full h-full bg-red-500 flex flex-row justify-center align-center">
+      {/* Introduction side */}
+      <div className="w-1/2 h-full bg-blue-500"></div>
+      {/* Image side */}
+      <div className="w-1/2 h-full bg-emerald-500"></div>
+    </div>
+  );
+};
+
+export default HomeHeader;
